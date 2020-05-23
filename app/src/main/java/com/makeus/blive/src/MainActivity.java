fc7f785e55c2dfa32bccbef7433ff8d970474c00
윤대현 <ydh0213@gmail.com>
2020-05-23 14:27:16 +0900

    Home 탭 상단 타이틀 및 알림 버튼 배치

--- app/src/main/java/com/makeus/blive/src/MainActivity.java
+++ app/src/main/java/com/makeus/blive/src/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setView();
+
+        // TODO: test code. Remove it later
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        Log.d("BLIVE", "density: " + metrics.density);
+        Log.d("BLIVE", "densityDpi: " + metrics.densityDpi);
     }
 
     private void setView() {
