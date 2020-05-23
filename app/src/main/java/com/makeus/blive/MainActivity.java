a5239f541e2b6e80b7bcc3d7711fe79700eea86d
윤대현 <ydh0213@gmail.com>
2020-05-23 12:42:24 +0900

    Home Fragment 세팅

--- app/src/main/java/com/makeus/blive/MainActivity.java
+++ app/src/main/java/com/makeus/blive/MainActivity.java
@@ -1,12 +1,18 @@
 package com.makeus.blive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.makeus.blive.tab_home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
     private void setView() {
         BottomNavigationView botNav = findViewById(R.id.main_bot_nav);
         botNav.setItemIconTintList(null);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_frame_layout_main_screen,
+                HomeFragment.newInstance()).commitAllowingStateLoss();
+
+        botNav.setOnNavigationItemSelectedListener(new MainBotNavItemSelectedListener());
+    }
+
+    private class MainBotNavItemSelectedListener
+            implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.item_home: {
+                    transaction.replace(R.id.main_frame_layout_main_screen,
+                            HomeFragment.newInstance()).commitAllowingStateLoss();
+                    break;
+                }
+            }
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/makeus/blive/tab_home/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.makeus.blive.tab_home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.makeus.blive.R;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() { return new HomeFragment(); }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
